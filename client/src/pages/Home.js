846a2642c42f1586ae30059099527e8f2ded8542
alitemel89 <alitemel89.ce@gmail.com>
2022-02-19 20:20:36 +0300

    update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import logo from '../images/fbe.jpg';
 
 const Home = () => {
     return (
-        <div className='container'>
-            <h1>Dokuz Eylül Üniversitesi Fen Bilimleri Enstitüsü</h1>
+        <div className='text-center'>
+            <h2 className="mt-4">Dokuz Eylül Üniversitesi Fen Bilimleri Enstitüsü</h2>
+            <img src={logo} alt="Enstitü fotoğrafı" className='m-4' />
+            <p className='mt-4'>Enstitü Müdürü: Prof. Dr. Okan Fıstıkoğlu</p>
         </div>
     )
 }
