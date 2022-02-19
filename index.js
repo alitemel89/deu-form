299d82c2088467030d9cc6e649115976ceb53969
alitemel89 <alitemel89.ce@gmail.com>
2022-02-19 19:55:46 +0300

    update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`))
 
