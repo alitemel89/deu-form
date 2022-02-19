f2617e976b0c7f243f6f5a79fe3afea42a94f451
alitemel89 <alitemel89.ce@gmail.com>
2022-02-19 20:52:24 +0300

    update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
   password: "myPassword",
-  host: "localhost",
+  host: "https://dry-dawn-24525.herokuapp.com",
   port: 5432,
   database: "arge",
   connectionString: process.env.DATABASE_URL,
