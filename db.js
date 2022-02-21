7a2ec00802edf7cbb1a6b7b40a9a2b1ca5e4166b
alitemel89 <alitemel89.ce@gmail.com>
2022-02-21 17:36:02 +0300

    update db.js

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const Pool = require("pg").Pool;
 
 
 const pool = new Pool({
-  user: "pojspgykcojqrl",
-  password: "7890ad3435ee35b68018866afdf436701fb9f9c9cd4ad24bfa74280feb36ec79",
-  host: "ec2-52-5-1-20.compute-1.amazonaws.com",
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
   port: 5432,
-  database: "d98tnrvckggm4g",
+  database: process.env.PG_DATABASE,
 });
 
 
